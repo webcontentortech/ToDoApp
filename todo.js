302d04faa43c6edc4f1283c842db5828c9847773
Shikha Choubey <shikha@webcontentor.com>
2016-07-14 17:47:32 +0530

    index.html+todo.js

--- todo.js
+++ todo.js
@@ -1,5 +1,22 @@
-$(document).ready(function(){
-    $(function() {
-        $( "#date" ).datepicker();
+$(document).ready(function() {
+    $("#main").hide();
+    $("#main1").hide();
+    $("#date").datepicker({
+        yearRange: "2016:2020",
+        minDate: '0',
+        changeMonth: true,
+        changeYear: true,
+    });
+    $("#date1").datepicker({
+        yearRange: "2016:2020",
+        minDate: '0',
+        changeMonth: true,
+        changeYear: true,
+    });
+    $("#add").click(function() {
+        $("#main").dialog();
+    });
+    $("#edit").click(function() {
+        $("#main1").dialog();
     });
 });
