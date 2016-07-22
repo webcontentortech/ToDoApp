6fabc2f3b21d6ca0a834ad02d1990eec510d954d
Shikha Choubey <shikha@webcontentor.com>
2016-07-22 17:25:23 +0530

    incomplete

--- todo.js
+++ todo.js
@@ -1,11 +1,22 @@
-$(document).ready(function() {
-    var item={date:"",title:"",descrip:"",hour:"",min:"",sec:""};
+$(document).ready(function(){
+    var date;
+    var editHours;
+    var editminute;
+    var editDate;
+    var editTitle;
+    var editDescrip;
+    var hours;
+    var minutes;
     var items = [];
-    var newitems=[];
-    var addData={date:"",title:"",descrip:"",hour:"",min:"",sec:""};
-    var today = new Date();
-    var hours = today.getHours();
-    var minutes = today.getMinutes();
+    var latestItems=[];
+    var addData={date:"",title:"",descrip:"",hour:"",min:""};
+    var latestData={Hour:"",Min:"",Date:"",Title:"",Descrip:""};
+    var currentTime=new Date();
+    var alarmTime={H:currentTime.getHours(),M:currentTime.getMinutes()};
+    console.log(alarmTime);
+    setInterval(alarm(),1000);
+    startTime();
+    alarm();
     $("#main").hide();
     $("#date").datepicker({
         yearRange: "2016:2020",
@@ -17,27 +28,21 @@ $(document).ready(function() {
         $("#date").val("");
         $("#title").val("");
         $("#description").val("");
-        $("#h").val("");
-        $("#m").val("");
-        $("#s").val("");
         $.ui.dialog.prototype._focusTabbable = function() {};
         $("#main").dialog({
             buttons: {
                 SAVE: function() {
-                    var date=$("#date").val();
+                    date=$("#date").val();
                     var title=$("#title").val();
                     var descrip=$("#description").val();
-                    var hour=$("#h").text(hours);
-                    console.log(hour);
-                    var min=$("#m").val();
-                    var sec=$("#s").val();
-                    console.log(hour)
+                    var hour=$("#addStartHours").val();
+                    var min=$("#addStartMin").val();
+                    console.log(hour,min);
                     addData.date = date;
                     addData.title = title;
                     addData.descrip = descrip;
                     addData.hour=hour;
                     addData.min=min;
-                    addData.sec=sec;
                     console.log(addData);
                     items.push(addData);
                     $(this).dialog("close");
@@ -50,7 +55,7 @@ $(document).ready(function() {
     var loadDataInDiv = function(items){
         var div = '';
         $.each(items, function(key, value){
-            div='<body><table><tr><td style="color:red;width:100px;height:auto; padding:auto;border-style:solid;border-color:red;" title="Edit" id="div1"><input type="text" value='+value.date+'><br><input type="text" value='+value.title+'><br><textarea rows="" cols="">'+value.descrip+'</textarea>';
+            div='<body><table><tr><td style="color:red;width:100px;height:auto; padding:auto;border-style:solid;border-color:red;" title="Edit" id="div1"><input type="number" min="0" max="23" size="0.5" placeholder="H" id="editHours" value='+value.hour+'><input type="number" min="0" max="59" size="0.5" placeholder="M" id="editminute" value='+value.min+'><br><input type="text"  id="editDate" value='+value.date+'><br><input type="text" id="editTitle" value='+value.title+'><br><textarea rows="" cols="" id="editDescrip">'+value.descrip+'</textarea>';
         });
         $(".box1").append(div);
         $("#div1").draggable();
@@ -58,11 +63,66 @@ $(document).ready(function() {
             $(this).dialog({
                 buttons:{
                     SAVE:function(){
+                        editHours=document.getElementById('editHours').value;
+                        editminute=document.getElementById('editminute').value;
+                        editDate=document.getElementById('editDate').value;
+                        editTitle=document.getElementById('editTitle').value;
+                        editDescrip=document.getElementById('editDescrip').value;
+                        latestData.Hour=editHours;
+                        latestData.Min=editminute;
+                        latestData.Date=editDate;
+                        latestData.Title=editTitle;
+                        latestData.Descrip=editDescrip;
+                        console.log(latestData);
+                        latestItems.push(latestData);
                         $(this).dialog("close");
-                        loadDataInDiv(item);
+                        loadNewInDiv(latestItems);
                     }
                 }
             });
+        });
+    }
+
+    var loadNewInDiv=function(latestItems) {
+        var newDiv='';
+        $.each(items, function(key, value){
+            newDiv='<body><table><tr><td style="color:red;width:100px;height:auto; padding:auto;border-style:solid;border-color:red;" title="Edit" id="div2"><input type="number" min="0" max="23" size="0.5" placeholder="H" id="editHours" value='+latestData.Hour+'><input type="number" min="0" max="59" size="0.5" placeholder="M" id="editminute" value='+latestData.Min+'><br><input type="text"  id="editDate" value='+latestData.Date+'><br><input type="text" id="editTitle" value='+latestData.Title+'><br><textarea rows="" cols="" id="editDescrip">'+latestData.Descrip+'</textarea>';
+        });
+        $(".box1").append(newDiv);
+        $("#div2").draggable();
+    }
+    
+    /*function editing(){
+    if(edit){
+        $("#div2").click(function(){
+            $(this).dialog({
+                buttons:{
+                    SAVE:function(){
+                        latestItems.push(latestData);
+                        $(this).dialog("close");
+                        loadNewInDiv(latestItems);
+                    }
+                }
+            });
+        });   
+    }else{
+
+    }
+    }*/
+
+    function startTime() {
+        hours = currentTime.getHours();
+        minutes = currentTime.getMinutes();
+        var timeHour=$("#addStartHours").val(hours);
+        var timeMin=$("#addStartMin").val(minutes);
+    }
+
+    function alarm(){
+        $.each(items, function(key, value){
+            if(alarmTime==currentTime){
+                console.log("hiii");
+                alert("time is over");
+            }
         });
     }
 });
