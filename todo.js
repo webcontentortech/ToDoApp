9061d18f831b3acb8ba1f262f0eb70254b0a5bae
Shikha Choubey <shikha@webcontentor.com>
2016-07-25 18:03:00 +0530

    alarm created

--- todo.js
+++ todo.js
@@ -1,4 +1,6 @@
 $(document).ready(function(){
+    var starthour;
+    var startmin;
     var date;
     var editHours;
     var editminute;
@@ -16,6 +18,7 @@ $(document).ready(function(){
     var currentTime;
     var alarmTime;
     startTime();
+    // alarm();
     $("#main").hide();
     $("#date").datepicker({
         yearRange: "2016:2020",
@@ -38,14 +41,14 @@ $(document).ready(function(){
                         date=$("#date").val();
                         var title=$("#title").val();
                         var descrip=$("#description").val();
-                        var hour=$("#addStartHours").val();
-                        var min=$("#addStartMin").val();
+                        starthour=$("#addStartHours").val();
+                        startmin=$("#addStartMin").val();
                         var ampm=$("#ampm").val();
                         addData.date = date;
                         addData.title = title;
                         addData.descrip = descrip;
-                        addData.hour=hour;
-                        addData.min=min;
+                        addData.hour=starthour;
+                        addData.min=startmin;
                         addData.AMPM=ampm;
                         console.log(addData);
                         items.push(addData);
@@ -60,7 +63,20 @@ $(document).ready(function(){
     var loadDataInDiv = function(items){
         var div = '';
         $.each(items, function(key, value){
-            div='<body><table><tr><td style="color:red;width:100px;height:auto; padding:auto;border-style:solid;border-color:red;" title="Edit" id="latesttask"><input type="number" min="0" max="23" size="0.5" placeholder="H" id="editHours" value='+value.hour+'><input type="number" min="0" max="59" size="0.5" placeholder="M" id="editminute" value='+value.min+'><input type="text" maxlength="2" size="4"  id="ampm" value='+value.AMPM+'><button id="alarm1" >AON</button><br><input type="text"  id="editDate" value='+value.date+'><br><input type="text" id="editTitle" value='+value.title+'><br><textarea rows="" cols="" id="editDescrip">'+value.descrip+'</textarea>';
+            div='<body>'
+                    +'<table>'
+                        +'<tr>'
+                            +'<td style="color:red;width:100px;height:auto; padding:auto;border-style:solid;border-color:red;" title="Edit" id="latesttask">'
+                                +'<input type="number" min="0" max="23" size="0.5" placeholder="H" id="editHours" value='+value.hour+'>'
+                                +'<input type="number" min="0" max="59" size="0.5" placeholder="M" id="editminute" value='+value.min+'>'
+                                +'<button id="alarm1" >AON</button>'
+                                +'<br>'
+                                +'<input type="text"  id="editDate" value='+value.date+'>'
+                                +'<br>'
+                                +'<input type="text" id="editTitle" value='+value.title+'>'
+                                +'<br>'
+                                +'<textarea rows="" cols="" id="editDescrip">'+value.descrip+'</textarea>'
+                            +'</td>';
         });
         $(".box1").append(div);
         $("#latesttask").draggable();
@@ -70,20 +86,22 @@ $(document).ready(function(){
                     SAVE:function(){
                         latestItems.push(latestData);
                         editHours=document.getElementById('editHours').value;
-                        editminute=document.getElementById('editminute').value;
+                        editMinute=document.getElementById('editminute').value;
                         editDate=document.getElementById('editDate').value;
                         editTitle=document.getElementById('editTitle').value;
                         editDescrip=document.getElementById('editDescrip').value;
                         ampm=document.getElementById('ampm').value;
                         latestData.Hour=editHours;
-                        latestData.Min=editminute;
+                        latestData.Min=editMinute;
                         latestData.AMPM=ampm;
                         latestData.Date=editDate;
                         latestData.Title=editTitle;
                         latestData.Descrip=editDescrip;
+                        if (today.getMinutes() == editMinute) {
+            alert("alarm ringing");
+        }
+                        // alarm();
                         console.log(latestData);
-                        currentTime={H:today.getHours(),M:today.getMinutes(),ampm:addData.AMPM};
-                        console.log(currentTime);
                         $(this).dialog("close");
                         loadNewInDiv(latestItems);
                     }
@@ -95,13 +113,21 @@ $(document).ready(function(){
     var loadNewInDiv=function(latestItems) {
         var newDiv='';
         $.each(items, function(key, value){
-            newDiv='<body><table><tr><td style="color:red;width:100px;height:auto; padding:auto;border-style:solid;border-color:red;" title="Edit" id="edittask"><input type="number" min="0" max="23" size="0.5" placeholder="H" id="editHours" value='+latestData.Hour+'><input type="number" min="0" max="59" size="0.5" placeholder="M" id="editminute" value='+latestData.Min+'><input type="text" maxlength="2" size="4"  id="ampm1" value='+latestData.AMPM+'><button id="alarm2" name="AOFF</button><br><input type="text"  id="editDate" value='+latestData.Date+'><br><input type="text" id="editTitle" value='+latestData.Title+'><br><textarea rows="" cols="" id="editDescrip">'+latestData.Descrip+'</textarea>';
-            if((latestData.Hour==currentTime.H)&&(latestData.Min==currentTime.M)){
-                console.log("hiii");
-                alert("time is over");
-            }else{
-                console.log("hello");
-            }
+            newDiv='<body>'
+            +'<table>'
+            +'<tr>'
+            +'<td style="color:red;width:100px;height:auto;padding:auto;border-style:solid;border-color:red;" title="Edit" id="edittask">'
+                +'<input type="number" min="0" max="23" size="0.5" placeholder="H" size="0.5" id="editHours" value='+latestData.Hour+'>'
+                +'<input type="number" min="0" max="59" size="0.5" placeholder="M" size="0.5" id="editminute" value='+latestData.Min+'>'
+                +'<input type="text" size="4" onChange="setv()" id="ampm1" value='+latestData.AMPM+'>'
+                +'<button id="alarm2" name="AOFF</button>'
+                +'<br>'
+                +'<input type="text"  id="editDate" value='+latestData.Date+'>'
+                +'<br>'
+                +'<input type="text" id="editTitle" value='+latestData.Title+'>'
+                +'<br>'
+                +'<textarea rows="" cols="" id="editDescrip">'+latestData.Descrip+'</textarea>'
+            +'</td>';
         });
         $(".box1").append(newDiv);
         $("#edittask").draggable();
@@ -115,4 +141,22 @@ $(document).ready(function(){
         var timeMin=$("#addStartMin").val(minutes);
         var AMPM=$("#ampm").val(ampm);
     }
+
+    /*function alarm(){
+        currentTime={H:today.getHours(),M:today.getMinutes(),ampm:addData.AMPM};
+        if((today.getHours()==editHours)&&(today.getMinutes()==editMinute)){
+                console.log("hiii");
+                alert("time is over");
+        }else{
+                console.log("hello");
+            }
+    }*/
+    // function alarm() {
+    //     var todayMin = today.getMinutes();
+    //     var newMin = parseInt(editminute);
+    //     console.log(todayMin,newMin);
+    //     if (todayMin == (newMin)) {
+    //         alert("alarm ringing");
+    //     }
+    // }
 });
